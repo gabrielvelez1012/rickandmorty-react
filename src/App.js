46af37baf8340f2574e842e5d51488c111d2a06b
gabrielv91 <gabrielvelez10121991@gmail.com>
2023-05-13 01:30:49 -0500

    second update

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Episodes from "./Pages/Episodes";
 import Location from "./Pages/Location";
+import CardDetails from "./components/Cards/CardDetails";
 
 
 function App () {
@@ -19,8 +20,12 @@ function App () {
       <Navbar />
       <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/:id" element={<CardDetails />} />
+
       <Route path="/episodes" element={<Episodes />} />
+      <Route path="/episodes/:id" element={<CardDetails />} />
       <Route path="/location" element={<Location />} />
+      <Route path="/location/:id" element={<CardDetails />} />
     </Routes>
     </Router>
   )
@@ -51,6 +56,8 @@ const Home = () => {
   return (
     <div className="App">
 
+      <h1 className="text-center fw-bold mb-4">Characters</h1>
+
         <Search setPageNumber={setPageNumber} setSearch={setSearch} />
 
         <div className="container">
@@ -65,7 +72,7 @@ const Home = () => {
             
             <div className="col-8">
               <div className="row">
-                <Cards results={results} />
+                <Cards page="/" results={results} />
               </div>
             </div>
           </div>
